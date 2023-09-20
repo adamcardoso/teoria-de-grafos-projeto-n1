efce84e9eecf81b5f92ac6d8597c2d255b5bfe28
Adam Cardoso <adamjuniorcardoso@gmail.com>
2023-09-20 14:07:13 -0300

    Criando projeto para resolução da Atividade N1 da Cadeira de Teoria de Grafos

---
+++ backend/uniritter/src/main/java/com/teoriadegrafos/uniritter/UniritterApplication.java
@@ -0,0 +1,13 @@
+package com.teoriadegrafos.uniritter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniritterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniritterApplication.class, args);
+	}
+
+}
---
+++ backend/uniritter/src/test/java/com/teoriadegrafos/uniritter/UniritterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teoriadegrafos.uniritter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniritterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
